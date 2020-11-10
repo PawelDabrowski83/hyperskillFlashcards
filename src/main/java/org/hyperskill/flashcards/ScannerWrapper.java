24b640dcfc153303f5785ba83cdb55812a58bedc
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-10 18:40:52 +0100

    removed redundant methods in ScannerWrapper

--- src/main/java/org/hyperskill/flashcards/ScannerWrapper.java
+++ src/main/java/org/hyperskill/flashcards/ScannerWrapper.java
@@ -1,6 +1,5 @@
 package org.hyperskill.flashcards;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ScannerWrapper implements AutoCloseable {
@@ -13,14 +12,6 @@ public class ScannerWrapper implements AutoCloseable {
 
     public String getString(){
         return scanner.nextLine().trim();
-    }
-
-    public int getInt() throws InputMismatchException {
-        return scanner.nextInt();
-    }
-
-    public double getDouble() throws InputMismatchException{
-        return scanner.nextDouble();
     }
 
     public boolean hasNextLine(){
